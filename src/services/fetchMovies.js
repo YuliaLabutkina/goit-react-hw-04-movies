e2afc2bca1daa550363026e71b9398cec923ea3f
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-12-26 21:56:04 +0200

    added hw-04-movies

--- src/services/fetchMovies.js
+++ src/services/fetchMovies.js
@@ -3,37 +3,35 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const apiKey = '81f248d3c9154788229a5419bb33091a';
 
+axios.defaults.params = {
+  api_key: apiKey,
+};
+
 const fetchMoviesGetTrending = async (page = 1) => {
-  const { data } = await axios.get(
-    `/trending/all/day?api_key=${apiKey}&page=${page}`,
-  );
+  const { data } = await axios.get(`/trending/all/day?page=${page}`);
   return data;
 };
 
 const fetchMoviesGetSearch = async (search, page = 1) => {
   const { data } = await axios.get(
-    `/search/movie?api_key=${apiKey}&language=en-US&page=${page}&include_adult=false&query=${search}`,
+    `/search/movie?language=en-US&page=${page}&include_adult=false&query=${search}`,
   );
   return data;
 };
 
 const fetchMoviesGetDetails = async movie_id => {
-  const { data } = await axios.get(
-    `/movie/${movie_id}?api_key=${apiKey}&language=en-US`,
-  );
+  const { data } = await axios.get(`/movie/${movie_id}?language=en-US`);
   return data;
 };
 
 const fetchMoviesGetActors = async movie_id => {
-  const { data } = await axios.get(
-    `/movie/${movie_id}/credits?api_key=${apiKey}&language=en-US`,
-  );
+  const { data } = await axios.get(`/movie/${movie_id}/credits?language=en-US`);
   return data;
 };
 
 const fetchMoviesGetReviews = async (movie_id, page = 1) => {
   const { data } = await axios.get(
-    `/movie/${movie_id}/reviews?api_key=${apiKey}&language=en-US&page=${page}`,
+    `/movie/${movie_id}/reviews?language=en-US&page=${page}`,
   );
   return data;
 };
